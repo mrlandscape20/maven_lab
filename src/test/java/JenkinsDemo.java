983adcb551a4ae32249cb53024e8524989ab85a9
Canh Nguyen <cnp@elca.vn>
2020-06-23 17:01:37 +0700

    Maven build test

---
+++ src/test/java/JenkinsDemo.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JenkinsDemo
+{
+    private Integer a = 1;
+    private Integer b = 2;
+    private Integer c;
+
+    @Before
+    public void calculation()
+    {	
+    	c = a + b;
+    }
+
+    @Test
+    public void testCasePassed()
+    {
+        Assert.assertEquals(3, c.intValue());
+        System.out.println("c is : " + c);
+    }
+
+    @Test
+    public void testCaseFailed()
+    {
+        Assert.assertEquals(4, c.intValue());
+        System.out.println("c is : " + c);
+    }
+
+}
